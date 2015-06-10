f5296b096be38e14a23a124839bae7914423d08f
JaeHee Cho <cho15255@gmail.com>
2015-06-09 23:16:12 -0400

    implemented FavouriteInfoSessionViewController

--- WATjob/WATjob/FavouriteInfoSessionListViewController.swift
+++ WATjob/WATjob/FavouriteInfoSessionListViewController.swift
@@ -9,6 +9,48 @@
 import Foundation
 import UIKit
 
-class FavouriteInfoSessionListViewController: UIViewController {
+class FavouriteInfoSessionListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var tableView: UITableView!
+    var infoSessionList: Array<InfoSession>;
+    
+    required init(coder aDecoder: NSCoder) {
+        self.infoSessionList = []
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DataCenter.getFavouritedInfoSessionList { (results) -> () in
+            self.infoSessionList = DataCenter.arrayFromResults(results!)
+            self.tableView.reloadData()
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return infoSessionList.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("InfoSessionListCell", forIndexPath: indexPath) as! InfoSessionListCell;
+        
+        var infoSession = infoSessionList[indexPath.row];
+        cell.employerLabel.text = infoSession.employer;
+        cell.startTimeLabel.text = infoSession.startTime;
+        cell.endTimeLabel.text = infoSession.endTime;
+        cell.locationLabel.text = infoSession.location;
+        cell.favouriteButton.tag = indexPath.row
+        cell.favouriteButton.addTarget(self, action: "favouriteClicked:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        return cell;
+    }
+    
+    func favouriteClicked(sender: UIButton) -> Void {
+        // use the tag to mark the database.
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
 }
